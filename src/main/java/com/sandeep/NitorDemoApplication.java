205b5fa6ea24e598520aaf25a5bc07aeefe20514
sandeep <sandeeppatil776@gmail.com>
2023-01-03 10:57:35 +0530

    feature-008 newly updated

--- src/main/java/com/sandeep/NitorDemoApplication.java
+++ src/main/java/com/sandeep/NitorDemoApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NitorDemoApplication {
  
    public static int a=100;
-   public String name = "Sandeep";         
+   public String name = "Sandeep";   
+   public boolean b= true;      
 	public static void main(String[] args) {
 		SpringApplication.run(NitorDemoApplication.class, args);
 	}
